d3e90b31617a7afaf5f7ac22a6f429f424c96bad
CamilCar <cc.carsbo@gmail.com>
2023-03-19 10:24:48 +0000

    Add cal. to booking page, change href names in nav

---
+++ booking/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+import datetime
+
+
+class StartDateInput(forms.DateInput):
+    input_type = 'date'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.attrs.setdefault('min', datetime.date.today() + datetime.timedelta(days=1))
+
+
+class EndDateInput(forms.DateInput):
+    input_type = 'date'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.attrs.setdefault('min', datetime.date.today() + datetime.timedelta(days=2))
+
+
+
+class BookingForm(forms.Form):
+    start_date = forms.DateField(widget=StartDateInput)
+    end_date = forms.DateField(widget=EndDateInput)
+    amount_adults = forms.IntegerField(min_value=1, initial=1)
+    amount_kids = forms.IntegerField(required=False)
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from .forms import BookingForm
 
 # Create your views here.
 
 
 def booking_page(request):
+    booking_form = BookingForm()
+    print(booking_form)
 
-    return render(request, "booking/booking.html")
+    return render(request, "booking/booking.html", {
+        "booking_form": booking_form
+})
+
+
+def reservation_summary(request):
+    return render(request, "booking/reservation_summary.html")
+
+
+@login_required
+def new_booking(request):
+    render(request, 'booking/booking_successful.html')
